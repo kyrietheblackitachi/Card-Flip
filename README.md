fb496105e2e7c4d25212f61af3efd6c135c78a1e
kyrietheblackitachi <robertkyrie100@gmail.com>
2022-10-18 12:40:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Card-Flip
+ card flip game
---
+++ src/cards.js
@@ -0,0 +1,13 @@
+let cardList=[
+    {name:"Apple",src:"/images/apple.png"},
+    {name:"Php",src:"./images/applications-php.png"},
+    {name:"Github",src:"./images/github.png"},
+    {name:"Java",src:"./images/java.png"},
+    {name:"NodeJs",src:"./images/nodejs.png"},
+    {name:"Nvidia",src:"./images/nvidia.png"},
+    {name:"Python",src:"./images/python.png"},
+    {name:"Windows",src:"./images/windows.png"},
+    {name:"Twitter",src:"./images/twitter.png"},
+    {name:"XBox",src:"./images/XBox.png"}
+]
+export default cardList
